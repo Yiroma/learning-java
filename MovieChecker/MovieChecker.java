1b272eba7871c8b96a6815c6305ab13bcee129b3
Yiroma <yiromaric@gmail.com>
2025-11-17 16:46:50 +0100

    quest 03 : DONE

---
+++ MovieChecker/MovieChecker.java
@@ -0,0 +1,14 @@
+class MovieChecker {
+  
+  public static void main(String[] args) {
+    String movieName = "Indiana Jones et la dernière croisade";
+    boolean watched = true;
+    int releaseDate = 1989;
+    float rating = 8.2f;
+    
+    System.out.println("Name of the movie : " + movieName);
+    System.out.println("Already watched : " + watched);
+    System.out.println("Release date : " + releaseDate);
+    System.out.println("Rating : " + rating);
+  }
+}
