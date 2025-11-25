e667d2f2a4dee2493b1eb5ec0c7408202e2d137f
Yiroma <yiromaric@gmail.com>
2025-11-25 15:50:09 +0100

    quest 8 : DONE

---
+++ 08-Fly/Fly.java
@@ -0,0 +1,17 @@
+public interface Fly {
+
+    // décoller
+    void takeOff();
+
+    // monter
+    int ascend(int meters);
+
+    // glisser
+    void glide();
+
+    // descendre
+    int descend(int meters);
+
+    // atterrir
+    void land();
+}
