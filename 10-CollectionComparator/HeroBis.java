31d47e730eb90635598ebcd120fbeb63bb8496af
Yiroma <yiromaric@gmail.com>
2025-11-30 18:03:58 +0100

    quest 10 : DONE

---
+++ 10-CollectionComparator/HeroBis.java
@@ -0,0 +1,35 @@
+public class HeroBis implements Comparable<HeroBis> {
+
+    // attributs
+    private String name;
+    private int age;
+
+    // constructeurs
+    public HeroBis(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // accesseurs (getters)
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    // mutateurs (setters)
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(HeroBis hero) {
+        return this.name.compareTo(hero.name);
+    }
+}
