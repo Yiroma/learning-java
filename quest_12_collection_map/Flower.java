9865def0f51167d978f06f53fb1b4d6c33dae4cf
Yiroma <yiromaric@gmail.com>
2025-12-02 12:30:46 +0100

    init quest 12

---
+++ quest_12_collection_map/Flower.java
@@ -0,0 +1,18 @@
+package quest_12_collection_map;
+
+public class Flower {
+
+    private String name;
+
+    public Flower(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
